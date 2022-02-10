ce9817ca9f91d380038d778a13bd3b9f4cc12872
gtarcea <glenn.tarcea@gmail.com>
2022-02-10 11:46:06 -0500

    Speculative change to show important process attributes

--- resources/views/partials/activities/activity-card.blade.php
+++ resources/views/partials/activities/activity-card.blade.php
@@ -1,6 +1,24 @@
 <div class="mt-2">
     <h5>
-        <a href="{{route('projects.activities.show', [$project, $activity])}}">{{$activity->name}}</a> <a class="ml-2" href="#">hide</a>
+        <a href="{{route('projects.activities.show', [$project, $activity])}}">{{$activity->name}}</a> <a class="ml-2"
+                                                                                                          href="#">hide</a>
+        @foreach($activity->attributes as $attribute)
+            @if(!is_null($attribute->marked_important_at))
+                {{$attribute->name}}:
+                @if(is_array($attribute->values[0]->val["value"]))
+                    @json($attribute->values[0]->val["value"], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+                @else
+                    @if(blank($attribute->values[0]->val["value"]))
+                        No value
+                    @else
+                        {{$attribute->values[0]->val["value"]}}
+                    @endif
+                @endif
+                @if($attribute->values[0]->unit != "")
+                    {{$attribute->values[0]->unit}}
+                @endif
+            @endif
+        @endforeach
     </h5>
     @if(!blank($activity->description))
         <form>
